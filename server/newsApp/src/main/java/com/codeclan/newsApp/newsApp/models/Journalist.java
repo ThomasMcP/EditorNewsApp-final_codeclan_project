13fbb8d4173cf6aaea05f3292ffdbf3739844748
Chris Jennings <chrisjennings555@gmail.com>
2019-02-02 08:59:04 +0000

    initial props written

--- server/newsApp/src/main/java/com/codeclan/newsApp/newsApp/models/Journalist.java
+++ server/newsApp/src/main/java/com/codeclan/newsApp/newsApp/models/Journalist.java
@@ -1,4 +1,26 @@
 package com.codeclan.newsApp.newsApp.models;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "journalists")
 public class Journalist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @JsonIgnoreProperties("journalists")
+    @OneToMany(mappedBy = "journalist", fetch = FetchType.LAZY)
+    private List<Article> articles;
+
+
 }
