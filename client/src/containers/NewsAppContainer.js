bd7180dea30f08b24badbec85f940ea6e20f4f12
Thomas McPhee <mcp.thom@gmail.com>
2019-02-03 12:28:21 +0000

    moved nav bar to own component

--- client/src/containers/NewsAppContainer.js
+++ client/src/containers/NewsAppContainer.js
@@ -1,27 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import MockData from '../data/mockdata.js';
 import ArticleList from '../components/ArticleList.js'
-
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-};
+import NavBar from '../components/NavBar.js';
 
 class NewsAppContainer extends Component {
   constructor(props){
@@ -32,24 +12,13 @@ class NewsAppContainer extends Component {
   }
 
   render(){
-    const classes = this.props.classes;
     return (
       <>
-      <AppBar position="static">
-       <Toolbar>
-         <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-           <MenuIcon />
-         </IconButton>
-         <Typography variant="h6" color="inherit" className={classes.grow}>
-           NewsApp
-         </Typography>
-         <Button color="inherit">Login</Button>
-       </Toolbar>
-     </AppBar>
+      <NavBar />
       <ArticleList articles={this.state.articles} />
       </>
     )
   }
 }
 
-export default withStyles(styles)(NewsAppContainer);
+export default NewsAppContainer;
