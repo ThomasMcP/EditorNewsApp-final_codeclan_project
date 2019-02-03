e1fd821c785600021660843082edf640d580bd24
Thomas McPhee <mcp.thom@gmail.com>
2019-02-03 14:55:52 +0000

    added edit-article-form

--- client/src/components/ArticleForm.js
+++ client/src/components/ArticleForm.js
@@ -10,6 +10,8 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import TextField from '@material-ui/core/TextField';
+
 
 const styles = theme => ({
   root: {
@@ -18,25 +20,29 @@ const styles = theme => ({
   },
   formControl: {
     margin: theme.spacing.unit,
-    minWidth: 120,
+    minWidth: 500,
   },
   selectEmpty: {
     marginTop: theme.spacing.unit * 2,
   },
 });
 
-class SimpleSelect extends React.Component {
-  state = {
-    age: '',
-    name: 'hai',
-    labelWidth: 0,
-  };
-
-  componentDidMount() {
-    this.setState({
-      labelWidth: ReactDOM.findDOMNode(this.InputLabelRef).offsetWidth,
-    });
+class ArticleForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      title: "",
+      date: "",
+      description: "",
+      imagelink: ""
+    };
   }
+
+  // componentDidMount() {
+  //   this.setState({
+  //     labelWidth: ReactDOM.findDOMNode(this.props.InputLabelRef).offsetWidth,
+  //   });
+  // }
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
@@ -48,30 +54,56 @@ class SimpleSelect extends React.Component {
     return (
       <form className={classes.root} autoComplete="off">
         <FormControl className={classes.formControl}>
-          <InputLabel htmlFor="age-simple">Age</InputLabel>
-          <Select
-            value={this.state.age}
-            onChange={this.handleChange}
-            inputProps={{
-              name: 'age',
-              id: 'age-simple',
-            }}
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>Ten</MenuItem>
-            <MenuItem value={20}>Twenty</MenuItem>
-            <MenuItem value={30}>Thirty</MenuItem>
-          </Select>
+          <TextField
+           id="outlined-full-width"
+           label="Title"
+           placeholder="Enter Article Title"
+           className={classes.textField}
+           margin="normal"
+           variant="outlined"
+         />
+         <TextField
+          id="outlined-with-placeholder"
+          label="Author"
+          placeholder="Enter Author"
+          className={classes.textField}
+          margin="normal"
+          variant="outlined"
+         />
+         <TextField
+          id="outlined-with-placeholder"
+          label="Date"
+          placeholder="Enter Date DD/MM/YYYY"
+          className={classes.textField}
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+         id="outlined-textarea"
+         label="Description"
+         placeholder="Enter Description"
+         multiline
+         rows="4"
+         className={classes.textField}
+         margin="normal"
+         variant="outlined"
+       />
+       <TextField
+        id="outlined-with-placeholder"
+        label="ImageLink"
+        placeholder="Enter ImageLink"
+        className={classes.textField}
+        margin="normal"
+        variant="outlined"
+       />
         </FormControl>
       </form>
     );
   }
 }
 
-SimpleSelect.propTypes = {
+ArticleForm.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(SimpleSelect);
+export default withStyles(styles)(ArticleForm);
--- client/src/containers/Main.js
+++ client/src/containers/Main.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import React, { Component, Fragment } from 'react';
 import MockData from '../data/mockdata';
 import NewsAppContainer from './NewsAppContainer';
+import ArticleForm from '../components/ArticleForm.js';
 
 class Main extends Component {
 
@@ -14,6 +15,7 @@ class Main extends Component {
       <Router>
         <Fragment>
           <Route exact path="/" component={NewsAppContainer} />
+          <Route path="/edit-article" component={ArticleForm} />
         </Fragment>
       </Router>
     );
