e81bcd6968f18bb91634066ded62fd6f2adfdf0e
Chris Jennings <chrisjennings555@gmail.com>
2019-02-04 10:48:00 +0000

    controller updated

--- server/newsApp/src/main/java/com/codeclan/newsApp/newsApp/controllers/JournalistController.java
+++ server/newsApp/src/main/java/com/codeclan/newsApp/newsApp/controllers/JournalistController.java
@@ -1,4 +1,29 @@
 package com.codeclan.newsApp.newsApp.controllers;
 
+import com.codeclan.newsApp.newsApp.models.Journalist;
+import com.codeclan.newsApp.newsApp.repositories.ArticleRepository.ArticleRepository;
+import com.codeclan.newsApp.newsApp.repositories.JournalistRepository.JournalistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/journalists")
 public class JournalistController {
+
+    @Autowired
+    JournalistRepository journalistRepository;
+
+    @Autowired
+    ArticleRepository articleRepository;
+
+    @GetMapping(value = "/{id}")
+    public List<Journalist> getJournalistsWithName(@PathVariable Long id){
+        return journalistRepository.findJournalistById(id);
+    }
+
 }
