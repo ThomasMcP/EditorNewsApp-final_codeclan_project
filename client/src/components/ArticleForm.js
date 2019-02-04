d5f43fe20a66c2e40452972b2742e504df42f4b0
Steven Kay <stevendkay@gmail.com>
2019-02-04 13:45:32 +0000

    close button on article takes you back to /

--- client/src/components/ArticleForm.js
+++ client/src/components/ArticleForm.js
@@ -5,6 +5,7 @@ import FormControl from '@material-ui/core/FormControl';
 import Button from '@material-ui/core/Button';
 //import Select from '@material-ui/core/Select';
 import TextField from '@material-ui/core/TextField';
+import { Redirect } from 'react-router'
 
 
 const styles = theme => ({
@@ -32,7 +33,8 @@ class ArticleForm extends React.Component {
     console.dir(article);
 
     this.state = {
-      article: article
+      article: article,
+      redirectClose: false
     };
   }
 
@@ -40,9 +42,24 @@ class ArticleForm extends React.Component {
     this.setState({ [event.target.name]: event.target.value });
   };
 
+  handleClose(article){
+    console.log("In Author Edit");
+    this.setState({redirectClose:true});
+  }
+
   render() {
     const { classes } = this.props;
-
+    const { redirectClose } = this.state;
+    if (redirectClose) {
+      return(
+        <Redirect
+          to={{
+            pathname: "/",
+            search: "",
+          }}
+        />
+      )
+    }
     return (
       <form className={classes.root} autoComplete="off">
         <FormControl className={classes.formControl}>
@@ -108,7 +125,12 @@ class ArticleForm extends React.Component {
         <Button variant="contained" color="primary" className="button">
           Save
         </Button>
-        <Button variant="contained" color="secondary" className="button">
+        <Button
+          variant="contained"
+          color="secondary"
+          className="button"
+          onClick={() => this.handleClose()}
+        >
           Cancel
         </Button>
       </form>
