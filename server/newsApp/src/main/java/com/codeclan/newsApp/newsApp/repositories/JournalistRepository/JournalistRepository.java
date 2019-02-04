db67eb3dd8806c5eeb6fde4fc20ca963af317fd7
Chris Jennings <chrisjennings555@gmail.com>
2019-02-04 10:49:02 +0000

    journalist repo update

--- server/newsApp/src/main/java/com/codeclan/newsApp/newsApp/repositories/JournalistRepository/JournalistRepository.java
+++ server/newsApp/src/main/java/com/codeclan/newsApp/newsApp/repositories/JournalistRepository/JournalistRepository.java
@@ -6,5 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface JournalistRepository extends JpaRepository<Journalist, Long>, JournalistRepositoryCustom {
-    List<Journalist> findJournalistsByName(String name);
+
+    List<Journalist> findJournalistByName(String name);
+
+    List<Journalist> findJournalistById(Long journalistId);
+
 }
