97265fcec253187c2e35466391f6d927317af28c
Steven Kay <stevendkay@gmail.com>
2019-02-05 10:29:47 +0000

    now dataloader reading json seed file

--- server/newsApp/src/main/java/com/codeclan/newsApp/newsApp/NewsAppApplication.java
+++ server/newsApp/src/main/java/com/codeclan/newsApp/newsApp/NewsAppApplication.java
@@ -1,13 +1,19 @@
 package com.codeclan.newsApp.newsApp;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class NewsAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(NewsAppApplication.class, args);
+
 	}
 
 }
--- server/newsApp/src/main/java/com/codeclan/newsApp/newsApp/components/DataLoader.java
+++ server/newsApp/src/main/java/com/codeclan/newsApp/newsApp/components/DataLoader.java
@@ -10,6 +10,9 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.io.*;
+import java.nio.file.Paths;
+
 import java.util.Date;
 import java.util.GregorianCalendar;
 
@@ -28,41 +31,60 @@ public class DataLoader implements ApplicationRunner {
     }
 
     //    title; journalist; content;date; url;image_url;
-    public void run(ApplicationArguments args) {
+    public void run(ApplicationArguments args) throws IOException {
 
-        Journalist steven = new Journalist("Steven");
-        journalistRepository.save(steven);
+        // find file in same directory as this file
+        String currPath = Paths.get(".").toAbsolutePath().normalize().toString();
+        currPath += "/src/main/java/com/codeclan/newsApp/newsApp/components/mockdata.json";
+        System.out.println(currPath);
 
-        Journalist chris = new Journalist("Chris");
-        journalistRepository.save(chris);
+        // read file into string
 
-        Journalist tom = new Journalist("Tom");
-        journalistRepository.save(tom);
+        File file = new File(currPath);
+        FileInputStream fis = new FileInputStream(file);
+        InputStreamReader isr = new InputStreamReader(fis);
+        BufferedReader br = new BufferedReader(isr);
 
-        Journalist david = new Journalist("David");
-        journalistRepository.save(david);
+        String line;
+        while((line = br.readLine()) != null){
+            //process the line
+            System.out.println(line);
+        }
+        br.close();
 
-        GregorianCalendar date1 = new GregorianCalendar(2018, 10, 5);
-        GregorianCalendar date2 = new GregorianCalendar(1999, 12, 12);
-        GregorianCalendar date3 = new GregorianCalendar(2005, 8, 13);
-        GregorianCalendar date4 = new GregorianCalendar(2019, 02, 3);
-
-
-        Article article1 = new Article("Tendril Nostrils", "The investigation into the Bog Monster", "http:bbc.co.uk/feed/", "http:src.image_placeholder", date1, "description here", david);
-        articleRepository.save(article1);
-
-        Article article2 = new Article("Tech News", "Tech content", "www.tech.com", "image.tech.org", date2, "tech description", chris);
-        articleRepository.save(article2);
-
-        Article article3 = new Article("Sports News", "Sports Content", "www.sports.com", "image.sports.org", date3, "sports description", steven);
-        articleRepository.save(article3);
-
-        Article article4 = new Article("Science News", "Science Content", "www.science.com", "image.science.org", date4, "science description", tom);
-        articleRepository.save(article4);
-
-        Article article5 = new Article("Business News", "Business Content", "www.Business.com", "image.Business.org", date3, "Business description", steven);
-        articleRepository.save(article5);
-
+//        Journalist steven = new Journalist("Steven");
+//        journalistRepository.save(steven);
+//
+//        Journalist chris = new Journalist("Chris");
+//        journalistRepository.save(chris);
+//
+//        Journalist tom = new Journalist("Tom");
+//        journalistRepository.save(tom);
+//
+//        Journalist david = new Journalist("David");
+//        journalistRepository.save(david);
+//
+//        GregorianCalendar date1 = new GregorianCalendar(2018, 10, 5);
+//        GregorianCalendar date2 = new GregorianCalendar(1999, 12, 12);
+//        GregorianCalendar date3 = new GregorianCalendar(2005, 8, 13);
+//        GregorianCalendar date4 = new GregorianCalendar(2019, 02, 3);
+//
+//
+//        Article article1 = new Article("Tendril Nostrils", "The investigation into the Bog Monster", "http:bbc.co.uk/feed/", "http:src.image_placeholder", date1, "description here", david);
+//        articleRepository.save(article1);
+//
+//        Article article2 = new Article("Tech News", "Tech content", "www.tech.com", "image.tech.org", date2, "tech description", chris);
+//        articleRepository.save(article2);
+//
+//        Article article3 = new Article("Sports News", "Sports Content", "www.sports.com", "image.sports.org", date3, "sports description", steven);
+//        articleRepository.save(article3);
+//
+//        Article article4 = new Article("Science News", "Science Content", "www.science.com", "image.science.org", date4, "science description", tom);
+//        articleRepository.save(article4);
+//
+//        Article article5 = new Article("Business News", "Business Content", "www.Business.com", "image.Business.org", date3, "Business description", steven);
+//        articleRepository.save(article5);
+//
 
 
 
