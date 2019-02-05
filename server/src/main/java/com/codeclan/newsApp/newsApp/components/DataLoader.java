6e53eef8930c07d249d24dc3ed5101557b91292a
Chris Jennings <chrisjennings555@gmail.com>
2019-02-05 16:17:24 +0000

    hardcode image kitten into DL

--- server/src/main/java/com/codeclan/newsApp/newsApp/components/DataLoader.java
+++ server/src/main/java/com/codeclan/newsApp/newsApp/components/DataLoader.java
@@ -50,7 +50,7 @@ public class DataLoader implements ApplicationRunner {
         Article article1 = new Article("Tendril Nostrils", "The investigation into the Bog Monster", "http:bbc.co.uk/feed/", "http:src.image_placeholder", date1, "description here", david);
         articleRepository.save(article1);
 
-        Article article2 = new Article("Tech News", "Tech content", "www.tech.com", "image.tech.org", date2, "tech description", chris);
+        Article article2 = new Article("Tech News", "Tech content", "www.tech.com", "http://placekitten.com/200/300", date2, "tech description", chris);
         articleRepository.save(article2);
 
         Article article3 = new Article("Sports News", "Sports Content", "www.sports.com", "image.sports.org", date3, "sports description", steven);
