f0eac0a7d44a1d4f97718abf4206b4cb5b9f4081
Thomas McPhee <mcp.thom@gmail.com>
2019-02-06 15:44:20 +0000

    fixed get from api

--- client/src/proxy/server.js
+++ client/src/proxy/server.js
@@ -10,9 +10,34 @@ app.use(function (req, res, next) {
   next();
 });
 
+
+
+app.put("/articles/:id", (req, res, next) => {
+  console.log("in proxy PUT");
+  console.log(req.params);
+  const id = req.params['id'];
+  return fetch(`http://localhost:8080/api/articles/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  .then((result) => {
+    console.log(result);
+    return result.json()
+  })
+  .then((data) => {
+    console.log(data)
+    res.redirect('/')
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+})
+
 app.get("/", (req,res, next) => {
   console.log("In proxy")
-  return fetch('http://localhost:8080/api/articles', {
+  return fetch('http://localhost:8080/articles', {
      method: "GET",
      headers: {
        "Content-Type": "application/json"
@@ -24,7 +49,7 @@ app.get("/", (req,res, next) => {
   })
   .then((data) => {
     console.log(data)
-    res.send(JSON.stringify(data._embedded.articles))
+    res.send(JSON.stringify(data))
   })
   .catch((err) => {
     console.log(err);
