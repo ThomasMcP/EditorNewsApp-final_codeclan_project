17d83296a3b7d60c5ee5aa20e0402ef3cc81ef82
Chris Jennings <chrisjennings555@gmail.com>
2019-02-06 15:13:56 +0000

    renamed paths

--- server/src/main/java/com/codeclan/newsApp/newsApp/controllers/JournalistController.java
+++ server/src/main/java/com/codeclan/newsApp/newsApp/controllers/JournalistController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/journalists")
+@RequestMapping(value = "/api/journalists")
 public class JournalistController {
 
     @Autowired
